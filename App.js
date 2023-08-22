9746d3c13cace16a92094d4fc6adc56e24e0271e
Valeriu <valeriu.bahov@redspace.com>
2023-08-22 16:58:04 -0300

    Setup initial tabs navigation with hardcoded login

--- App.js
+++ App.js
@@ -9,17 +9,68 @@
 import React, {useEffect} from 'react';
 import Splash from './src/screens/auth/Splash';
 import Signup from './src/screens/auth/Signup';
+import Home from './src/screens/app/Home';
+import Favorites from './src/screens/app/Favorites';
+import Profile from './src/screens/app/Profile';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import Config from 'react-native-config';
 import Signin from './src/screens/auth/Signin';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
 import {colors} from './src/utils/colors';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {Image} from 'react-native';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => (
+  <Tab.Navigator
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let icon;
+
+        switch (route.name) {
+          case 'Home':
+            icon = focused
+              ? require('./src/assets/tabs/home_active.png')
+              : require('./src/assets/tabs/home.png');
+            break;
+          case 'Profile':
+            icon = focused
+              ? require('./src/assets/tabs/profile_active.png')
+              : require('./src/assets/tabs/profile.png');
+            break;
+          case 'Favorites':
+            icon = focused
+              ? require('./src/assets/tabs/bookmark_active.png')
+              : require('./src/assets/tabs/bookmark.png');
+            break;
+          default:
+            break;
+        }
+
+        // You can return any component that you like here!
+        return <Image style={{width: 24, height: 24}} source={icon} />;
+      },
+      headerShown: false,
+      tabBarShowLabel: false,
+      tabBarStyle: {
+        borderTopColor: colors.lightGrey,
+        backgroundColor: colors.white,
+      },
+    })}>
+    <Tab.Screen name="Home" component={Home} />
+    <Tab.Screen name="Favorites" component={Favorites} />
+    <Tab.Screen name="Profile" component={Profile} />
+  </Tab.Navigator>
+);
 
 const App = () => {
+  const isSignedIn = false;
+
   useEffect(() => {
     GoogleSignin.configure({
       scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
@@ -40,21 +91,31 @@ const App = () => {
     <SafeAreaProvider>
       <NavigationContainer theme={theme}>
         <Stack.Navigator>
-          <Stack.Screen
-            name="Splash"
-            component={Splash}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Signin"
-            component={Signin}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Signup"
-            component={Signup}
-            options={{headerShown: false}}
-          />
+          {isSignedIn ? (
+            <Stack.Screen
+              name="Tabs"
+              component={Tabs}
+              options={{headerShown: false}}
+            />
+          ) : (
+            <>
+              <Stack.Screen
+                name="Splash"
+                component={Splash}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen
+                name="Signin"
+                component={Signin}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen
+                name="Signup"
+                component={Signup}
+                options={{headerShown: false}}
+              />
+            </>
+          )}
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
