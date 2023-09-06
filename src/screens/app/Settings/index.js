15cbe46159682266e4cc0d37f1235fb591be9de7
Valeriu <valeriu.bahov@redspace.com>
2023-09-06 12:53:37 -0300

    Added settings page and redirect to URL

--- src/screens/app/Settings/index.js
+++ src/screens/app/Settings/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import {ScrollView, Text} from 'react-native';
+import {Linking, ScrollView, Text} from 'react-native';
 import {styles} from './styles';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Header from '../../../components/Header';
+import ListItem from '../../../components/ListItem';
 
 const Settings = () => {
+  const onItemPress = () => {
+    // simple redirect
+    Linking.openURL('https://google.com');
+  };
+
   return (
     <SafeAreaView>
+      <Header title="Settings" />
+
       <ScrollView style={styles.container}>
-        <Text>Settings</Text>
+        <Text style={styles.sectionTitle}>Help</Text>
+        <ListItem onPress={onItemPress} style={styles.item} title="FAQ" />
+        <ListItem
+          onPress={onItemPress}
+          style={styles.item}
+          title="Contact Us"
+        />
+        <ListItem
+          onPress={onItemPress}
+          style={styles.item}
+          title="Privacy & Terms"
+        />
       </ScrollView>
     </SafeAreaView>
   );
