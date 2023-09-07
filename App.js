dc7351b2d5da37548b3a539f0dfaa9dc244ce1b3
Valeriu <valeriu.bahov@redspace.com>
2023-09-07 16:28:58 -0300

    Added Listicle API to register the user, global state will be added next

--- App.js
+++ App.js
@@ -100,7 +100,7 @@ const Tabs = () => (
 );
 
 const App = () => {
-  const isSignedIn = true;
+  const isSignedIn = false;
 
   useEffect(() => {
     GoogleSignin.configure({
--- src/screens/auth/Signup/index.js
+++ src/screens/auth/Signup/index.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {styles} from './styles';
 import AuthHeader from '../../../components/AuthHeader';
-import {ScrollView, Text, View} from 'react-native';
+import {Alert, ScrollView, Text, View} from 'react-native';
 import Input from '../../../components/Input';
 import Checkbox from '../../../components/Checkbox';
 import Button from '../../../components/Button';
@@ -9,8 +9,11 @@ import Separator from '../../../components/Separator';
 import GoogleLogin from '../../../components/GoogleLogin';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
+import {request} from '../../../utils/request';
+
 const Signup = ({navigation}) => {
   const [checked, setChecked] = useState(false);
+  const [values, setValues] = useState({});
 
   const onCheck = () => {
     setChecked(!checked);
@@ -24,14 +27,75 @@ const Signup = ({navigation}) => {
     navigation.goBack();
   };
 
+  const onChange = (key, value) => {
+    setValues(v => ({...v, [key]: value}));
+  };
+
+  const onSubmit = async () => {
+    try {
+      if (
+        !values?.password ||
+        !values?.confirmPassword ||
+        !values?.fullName ||
+        !values?.email
+      ) {
+        Alert.alert('All fields are required');
+        return;
+      }
+
+      if (values?.password !== values?.confirmPassword) {
+        Alert.alert('Passwords do not match');
+        return;
+      }
+
+      if (!checked) {
+        Alert.alert('Please agree to the terms');
+        return;
+      }
+
+      const response = await request({
+        url: '/user/register',
+        method: 'POST',
+        data: values,
+      });
+
+      console.log(response);
+    } catch (err) {
+      console.log('error', err);
+    }
+  };
+
   return (
     <SafeAreaView>
       <ScrollView style={styles.container}>
         <AuthHeader onBackPress={onBack} title="Sign Up" />
 
-        <Input label="Name" placeholder="Cippo Pippo" />
-        <Input label="Email" placeholder="example@example.com" />
-        <Input isPassword label="Password" placeholder="********" />
+        <Input
+          value={values.fullName}
+          onChangeText={fullName => onChange('fullName', fullName)}
+          label="Name"
+          placeholder="Cippo Pippo"
+        />
+        <Input
+          value={values.email}
+          onChangeText={email => onChange('email', email)}
+          label="Email"
+          placeholder="example@example.com"
+        />
+        <Input
+          value={values.password}
+          onChangeText={pwd => onChange('password', pwd)}
+          isPassword
+          label="Password"
+          placeholder="********"
+        />
+        <Input
+          value={values.confirmPassword}
+          onChangeText={pwd => onChange('confirmPassword', pwd)}
+          isPassword
+          label="Confirm Password"
+          placeholder="********"
+        />
 
         <View style={styles.agreeRow}>
           <Checkbox checked={checked} onCheck={onCheck} />
@@ -41,7 +105,7 @@ const Signup = ({navigation}) => {
           </Text>
         </View>
 
-        <Button style={styles.button} title="Sign Up" />
+        <Button onPress={onSubmit} style={styles.button} title="Sign Up" />
 
         <Separator text="Or sign up with" />
 
